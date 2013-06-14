6575ac2b15fb27dd9421602cae35f878ed0adc6a
Denny C. Dai <dennycd@me.com>
2013-06-14 14:21:10 -0700

    initial commit of project source code

---
+++ README.md
@@ -0,0 +1,19 @@
+## A fast and high performance geospatial index and query library in C++
+
+
+
+
+## Usage
+[TBD]
+
+## Performance
+[TBD]
+
+## Road Map
+[TBD]
+
+## Author 
+Denny C. Dai <dennycd@me.com> or visit <http://dennycd.em>
+
+## License 
+The MIT License (MIT) <http://opensource.org/licenses/MIT>
---
+++ build/osx/geocached/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  geocached
+//
+//  Created by Denny C. Dai on 2013-06-14.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

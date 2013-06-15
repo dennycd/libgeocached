14649f5aa9168381cc5117edf4f389bcf8b7f845
Denny C. Dai <dennycd@me.com>
2013-06-14 17:28:51 -0700

    common data structure definition file

---
+++ src/share.hpp
@@ -0,0 +1,18 @@
+//
+//  share.hpp
+//  geocached
+//
+//  Created by Denny C. Dai on 2013-06-14.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#ifndef geocached_share_hpp
+#define geocached_share_hpp
+
+#include <string>
+#include <iostream>
+#include <functional>
+#include <unordered_map>
+#include <GeographicLib/Geodesic.hpp>
+
+#endif

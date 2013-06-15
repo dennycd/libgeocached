22bd8cd1fa6c09f3ce8f8dce7436e1dd20fe49db
Denny C. Dai <dennycd@me.com>
2013-06-14 17:29:22 -0700

    gtest unit testing cases

--- build/osx/geocached/main.cpp
+++ build/osx/geocached/main.cpp
@@ -7,12 +7,50 @@
 //
 
 #include <iostream>
+#include <gtest/gtest.h>
+#include <cell.hpp>
+#include <matrix.hpp>
+#include <geohash.h>
+
+#include <uuid/uuid.h>
+
+using namespace std;
+using namespace geocached;
+
+TEST(UUIDTest, test){
+    uuid_t myid;
+    uuid_generate(myid);
+    uuid_string_t myidStr;
+    uuid_unparse(myid, myidStr);
+    cout << "uuid is " << myidStr << endl;
+}
+
+TEST(Cell, test){
+    
+//    Cell<int> mycell("testid", 0,0, );
+    
+    
+}
+
+
+TEST(GeoHash, test){
+    char *hash = GEOHASH_encode(36.232, -123.324, 22);
+    cout << "geohash: " << hash << endl;
+    free(hash);
+}
+
+TEST(MatrixTest, test){
+    EXPECT_TRUE(true);
+}
+
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    #ifdef __cplusplus
+    cout << "cplusplus defined!" << endl;
+    #endif
+    
+    ::testing::InitGoogleTest(&argc, (char**)argv);
+    return RUN_ALL_TESTS();
 }
 
